88353738dc6db8daaff62ba0bfc348102e1f392e
Daniel W Hart <30510653+DanielW-Hart@users.noreply.github.com>
2019-04-15 15:03:06 -0600

    Create START_SCRIPT.ps1

---
+++ START_SCRIPT.ps1
@@ -0,0 +1,6 @@
+[int] $gpu_quantity = 1
+
+#My Windows PCs are named GILFOYLE-n, where n is a whole number
+[int] $pc_num = "$env:computername" -replace'GILFOYLE-',''
+
+Start-Process powershell -verb runas -ArgumentList "'./MINER_SWITCHER.ps1' -gpu_quantity $gpu_quantity -pc_num $pc_num"

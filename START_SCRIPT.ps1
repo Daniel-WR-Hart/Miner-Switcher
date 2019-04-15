100640dd44eee166f8ccec34f62666829160a8e0
Daniel W Hart <30510653+DanielW-Hart@users.noreply.github.com>
2019-04-15 15:14:42 -0600

    Launch the main script from here

--- START_SCRIPT.ps1
+++ START_SCRIPT.ps1
@@ -4,3 +4,4 @@
 [int] $pc_num = "$env:computername" -replace'GILFOYLE-',''
 
 Start-Process powershell -verb runas -ArgumentList "'./MINER_SWITCHER.ps1' -gpu_quantity $gpu_quantity -pc_num $pc_num"
+ 
